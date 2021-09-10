28c2d609d05a0c299862cad1f24edb750d0919fc
Taha <t2905j@gmail.com>
2021-09-10 19:51:32 +0530

    THA30 added

---
+++ THA30_31/tha30_31/src/redux/reducers/productReducer.js
@@ -0,0 +1,30 @@
+import { ActionTypes } from "../constants/action-types"
+
+
+const initialState = {
+    products:[
+        {
+            id:1,
+            title:"TAHA",
+            category:"programmer",
+        }
+    ],
+};
+export const productReducer = (state = initialState, {type, payload}) => {
+    switch(type){
+        case ActionTypes.SET_PRODUCTS:
+            return {...state, products:payload};
+        default:
+            return state;
+    }
+}
+export const selectedProductReducer = (state = {}, {type, payload}) => {
+    switch (type) {
+        case ActionTypes.SELECTED_PRODUCTS:
+            return {...state,...payload};
+            
+    
+        default:
+            return state;
+    }
+};
